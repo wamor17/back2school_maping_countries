da1f4341b2df13bc3b334a4c5391c70ecc234397
Walter Alejandro Moreno Ramírez <wa.morenoramirez@gmail.com>
2021-04-23 02:02:41 -0500

    Ordenamiento de scripts y css

---
+++ Resources/js/app.js
@@ -0,0 +1,863 @@
+AmCharts.makeChart("map",{
+	"type": "map",
+	"pathToImages": "http://www.amcharts.com/lib/3/images/",
+	"addClassNames": true,
+	"fontSize": 15,
+	"color": "#000000",
+	"projection": "miller",
+	"backgroundAlpha": 1,
+	"backgroundColor": "rgba(148, 204, 244, 0.2)",
+	"dataProvider": {
+		"map": "worldLow",
+		"getAreasFromMap": true,
+		"images": [
+			{
+				"top": 40,
+				"left": 60,
+				"width": 80,
+				"height": 40,
+				"pixelMapperLogo": true,
+				"imageURL": "http://pixelmap.amcharts.com/static/img/logo-black.svg no-active",
+				"url": "http://www.amcharts.com"
+			}
+		],
+		"areas": [
+			{
+				"id": "AE",
+				"title": "Emiratos Árabes Unidos",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "AL",
+				"title": "Alemania",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "AF",
+				"title": "Afganistán",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "AL",
+				"title": "Albania",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "AM",
+				"title": "Armenia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "AO",
+				"title": "Angola",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "AR",
+				"title": "Argentina",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "AT",
+				"title": "Austria",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "AU",
+				"title": "Australia",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "AZ",
+				"title": "Azerbaiyán",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "BA",
+				"title": "Bosnia y Herzegovina",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "BD",
+				"title": "Bangladesh",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "BE",
+				"title": "Bélgica",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "BF",
+				"title": "Burkina Faso",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "BG",
+				"title": "Bulgaria",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "BI",
+				"title": "Burundi",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "BJ",
+				"title": "Benin",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "BO",
+				"title": "Bolivia",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "BR",
+				"title": "Brasil",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "BT",
+				"title": "Bután",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "BW",
+				"title": "Botswana",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "BY",
+				"title": "Bielorrusia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "BZ",
+				"title": "Belice",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "CA",
+				"title": "Canadá",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "CD",
+				"title": "República Democrática del Congo",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "CF",
+				"title": "República Centroafricana",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "CG",
+				"title": "República del Congo",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "CH",
+				"title": "Suiza",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "CI",
+				"title": "Costa de Marfil",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "CL",
+				"title": "Chile",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "CM",
+				"title": "Camerún",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "CN",
+				"title": "China",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "CO",
+				"title": "Colombia",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "CR",
+				"title": "Costa Rica",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "CU",
+				"title": "Cuba",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "CZ",
+				"title": "Chequia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "DK",
+				"title": "Dinamarca",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "DO",
+				"title": "República Dominicana",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "DZ",
+				"title": "Argelia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "EC",
+				"title": "Ecuador",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "EE",
+				"title": "Estonia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "EG",
+				"title": "Egipto",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "EH",
+				"title": "Sahara Occidental",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "ER",
+				"title": "Eritrea",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "ES",
+				"title": "España",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "ET",
+				"title": "Etiopía",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "FK",
+				"title": "Islas Malvinas",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "FI",
+				"title": "Finlandia",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "FR",
+				"title": "Francia",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "GA",
+				"title": "Gabón",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "GB",
+				"title": "Reino Unido",
+				"color": "rgb(0, 24, 71, 1)"
+			},
+			{
+				"id": "GE",
+				"title": "Georgia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "GF",
+				"title": "Guyana Francesa",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "GH",
+				"title": "Ghana",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "GL",
+				"title": "Groenlandia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "GM",
+				"title": "Gambia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "GN",
+				"title": "Guinea",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "GQ",
+				"title": "Guinea Ecuatorial",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "GR",
+				"title": "Grecia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "GT",
+				"title": "Guatemala",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "GW",
+				"title": "Guinea-Bissau",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "GY",
+				"title": "Guyana",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "HN",
+				"title": "Honduras",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "HR",
+				"title": "Croacia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "HT",
+				"title": "Haití",
+				"color": "rgba(203,203,203,1)"
+			},
+			{
+				"id": "HU",
+				"title": "Hungría",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "ID",
+				"title": "Indonesia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "IE",
+				"title": "Irlanda",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "IN",
+				"title": "India",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "IQ",
+				"title": "Irak",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "IR",
+				"title": "Irán",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "IS",
+				"title": "Islandia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "IT",
+				"title": "Italia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "JO",
+				"title": "Jordania",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "JP",
+				"title": "Japón",
+				"color": "rgb(0, 24, 71, 1)"
+			},
+			{
+				"id": "KE",
+				"title": "Kenya",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "KG",
+				"title": "Kirguistán",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "KH",
+				"title": "Camboya",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "KP",
+				"title": "Corea del Norte",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "KR",
+				"title": "Corea del Sur",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "XK",
+				"title": "Kosovo",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "KZ",
+				"title": "Kazajstán",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "LA",
+				"title": "República Democrática Popular Lao",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "IL",
+				"title": "Israel",
+				"color": "rgb(0, 24, 71, 1)"
+			},
+			{
+				"id": "LK",
+				"title": "Sri Lanka",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "LR",
+				"title": "Liberia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "LS",
+				"title": "Lesoto",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "LT",
+				"title": "Lituania",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "LV",
+				"title": "Letonia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "LY",
+				"title": "Libia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "MA",
+				"title": "Marruecos",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "MD",
+				"title": "Moldavia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "ME",
+				"title": "Montenegro",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "MG",
+				"title": "Madagascar",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "MK",
+				"title": "Macedonia del Norte",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "ML",
+				"title": "Mali",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "MM",
+				"title": "Myanmar",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "MN",
+				"title": "Mongolia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "MR",
+				"title": "Mauritania",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "MW",
+				"title": "Malawi",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "MX",
+				"title": "México",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "MY",
+				"title": "Malasia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "MZ",
+				"title": "Mozambique",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "NA",
+				"title": "Namibia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "NE",
+				"title": "Niger",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "NG",
+				"title": "Nigeria",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "NI",
+				"title": "Nicaragua",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "NL",
+				"title": "Países Bajos",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "NO",
+				"title": "Noruega",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "NP",
+				"title": "Nepal",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "NZ",
+				"title": "Nueva Zelanda",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "OM",
+				"title": "Omán",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "PA",
+				"title": "Panamá",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "PE",
+				"title": "Perú",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "PG",
+				"title": "Papúa Nueva Guinea",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "PH",
+				"title": "Filipinas",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "PL",
+				"title": "Polonia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "PK",
+				"title": "Pakistán",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "PT",
+				"title": "Portugal",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "PY",
+				"title": "Paraguay",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "RO",
+				"title": "Rumania",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "RS",
+				"title": "Serbia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "RU",
+				"title": "Rusia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "RW",
+				"title": "Rwanda",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "SA",
+				"title": "Arabia Saudita",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "SD",
+				"title": "Sudan",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "SE",
+				"title": "Suecia",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "SI",
+				"title": "Slovenia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "SJ",
+				"title": "Svalbard y Jan Mayen",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "SK",
+				"title": "Eslovaquia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "SL",
+				"title": "Sierra Leona",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "SN",
+				"title": "Senegal",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "SO",
+				"title": "Somalia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "SR",
+				"title": "Suriname",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "SS",
+				"title": "Sudán del Sur",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "SV",
+				"title": "El Salvador",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "SY",
+				"title": "Siria",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "SZ",
+				"title": "Swazilandia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "TD",
+				"title": "Chad",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "TG",
+				"title": "Togo",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "TH",
+				"title": "Tailandia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "TJ",
+				"title": "Tayikistán",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "TM",
+				"title": "Turkmenistán",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "TN",
+				"title": "Túnez",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "TR",
+				"title": "Turquía",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "TW",
+				"title": "Taiwán",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "TZ",
+				"title": "Tanzania",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "UA",
+				"title": "Ucrania",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "UG",
+				"title": "Uganda",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "US",
+				"title": "Estados Unidos",
+				"color": "rgba(0, 24, 75, 1)"
+			},
+			{
+				"id": "UY",
+				"title": "Uruguay",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "UZ",
+				"title": "Uzbekistán",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "VE",
+				"title": "Venezuela",
+				"color": "rgba(210, 210, 210, 1)"
+			},
+			{
+				"id": "VN",
+				"title": "Vietnam",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "YE",
+				"title": "Yemen",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "ZA",
+				"title": "Sudáfrica",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "ZM",
+				"title": "Zambia",
+				"color": "rgb(210, 210, 210)"
+			},
+			{
+				"id": "ZW",
+				"title": "Zimbabwe",
+				"color": "rgb(210, 210, 210)"
+			}
+		]
+	},
+	"balloon": {
+		"horizontalPadding": 15,
+		"borderAlpha": 0,
+		"borderThickness": 1,
+		"verticalPadding": 15
+	},
+	"areasSettings": {
+		"color": "rgba(189,189,189,1)",
+		"outlineColor": "rgba(108, 108, 108, 1)",
+		"rollOverOutlineColor": "rgba(208, 208, 208, 1)",
+		"rollOverBrightness": 40,
+		"selectedBrightness": 40,
+		"selectable": true,
+		"unlistedAreasAlpha": 0,
+		"unlistedAreasOutlineAlpha": 0
+	},
+	"imagesSettings": {
+		"alpha": 1,
+		"color": "rgba(189,189,189,1)",
+		"outlineAlpha": 0,
+		"rollOverOutlineAlpha": 0,
+		"outlineColor": "rgba(208,208,208,0.12)",
+		"rollOverBrightness": 20,
+		"selectedBrightness": 20,
+		"selectable": true
+	},
+	"linesSettings": {
+		"color": "rgba(189, 189, 189, 1)",
+		"selectable": true,
+		"rollOverBrightness": 20,
+		"selectedBrightness": 20
+	},
+	"zoomControl": {
+		"zoomControlEnabled": true,
+		"homeButtonEnabled": true,
+		"panControlEnabled": false,
+		"right": 38,
+		"bottom": 30,
+		"minZoomLevel": 0.25,
+		"gridHeight": 100,
+		"gridAlpha": 0.1,
+		"gridBackgroundAlpha": 0,
+		"gridColor": "#FFFFFF",
+		"draggerAlpha": 1,
+		"buttonCornerRadius": 2
+	}
+});
